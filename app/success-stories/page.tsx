5839bfed166eebff79e3e5d8ff00154857b58806
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-03-16 08:58:16 -0700

    rr

---
+++ app/success-stories/page.tsx
@@ -0,0 +1,11 @@
+import SuccessStoriesPage from "@/components/stories/success-stories-page"
+
+export const metadata = {
+  title: "Success Stories | Ascent Consulting",
+  description: "Discover how we've helped our clients achieve success with our consulting services",
+}
+
+export default function Page() {
+  return <SuccessStoriesPage />
+}
+

8cdb9aefeee5b6b3cfed2c009ba9cc5cd2b11952
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-03-28 07:42:01 -0700

    mo 2244

--- components/ui/context-menu.tsx
+++ components/ui/context-menu.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import * as ContextMenuPrimitive from "@radix-ui/react-context-menu"
+import * as ContextMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { Check, ChevronRight, Circle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
